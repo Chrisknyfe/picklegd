1e18e5694049b9e6b434c70ab121971b0c7c1381
Chrisknyfe <chrisknyfe@gmail.com>
2024-01-05 20:30:32 -0800

    The real initial commit. Oh it's on like donkey kong.

---
+++ addons/picklemp/pickler.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name Pickler
+
+
+func pickle(obj, registry):
+	pass
+	
+func unpickle(datastruct, registry):
+	pass
---
+++ addons/picklemp/pickler_regsitry.gd
@@ -0,0 +1,13 @@
+extends Node
+class_name PicklerRegistry
+
+@export var picklers: Dictionary = {}
+
+func register(class_obj, pickler):
+	pass
+
+func pickle(obj):
+	pass
+	
+func unpickle(obj):
+	pass
