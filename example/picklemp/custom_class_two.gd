242e8d5998aab3b39305be360ba29f01807f6e4e
Chrisknyfe <chrisknyfe@gmail.com>
2024-04-21 21:41:45 -0700

    Working __getstate__ and __setstate__

--- example/picklemp/custom_class_two.gd
+++ example/picklemp/custom_class_two.gd
@@ -1,5 +1,19 @@
 extends Resource
 class_name CustomClassTwo
 
-var baz: float = 3.0
+var baz: float = 4.0
 var qux: String = "four point oh"
+var volatile_int: int = 3
+
+func __getstate__():
+	var state = {
+		"baz": baz,
+		"qux": qux
+	}
+	volatile_int = -1
+	return state
+	
+func __setstate__(state: Dictionary):
+	baz = state["baz"]
+	qux = state["qux"]
+	volatile_int = 99
