b196399ecaf1edbd1781c8fe298a68b4dafde3d0
Chrisknyfe <chrisknyfe@gmail.com>
2024-10-14 23:49:15 -0700

    add some data to the custom class

--- example/picklemp/picklestudio.gd
+++ example/picklemp/picklestudio.gd
@@ -8,9 +8,17 @@ func _ready():
 @rpc("any_peer") func send_pickle(pickled_data: PackedByteArray):
 	var form = pickler.unpickle(pickled_data)
 	$textbox.text = form.message
+	$HScrollBar.value = form.num_sheep
+	$CheckButton.button_pressed = form.is_reticulated
+	$ColorPickerButton.color = form.albedo
+	if typeof("asdf") == TYPE_STRING:
+		pass
 
 
 func _on_button_pressed() -> void:
 	var form = TestForm.new()
 	form.message = $textbox.text
+	form.num_sheep = $HScrollBar.value
+	form.is_reticulated = $CheckButton.button_pressed
+	form.albedo = $ColorPickerButton.color
 	send_pickle.rpc(pickler.pickle(form))
--- example/picklemp/testform.gd
+++ example/picklemp/testform.gd
@@ -4,3 +4,4 @@ extends Object
 @export var message: String = ""
 @export var is_reticulated: bool = false
 @export var num_sheep: int = 2
+@export var albedo: Color = Color.BLUE
