4c048c5aa0c3a01d01c518587ccbe4ef4bf49a5c
Chrisknyfe <chrisknyfe@gmail.com>
2024-10-14 23:16:08 -0700

    refactor mpstart

--- example/picklemp/mpstart.gd
+++ example/picklemp/mpstart.gd
@@ -1,20 +1,61 @@
 extends Control
 
-var server_scene: String = "res://example/picklemp/mpserver.tscn"
-var client_scene: String = "res://example/picklemp/mpclient.tscn"
+const DEFAULT_PORT = 28960
+const MAX_CLIENTS = 50
+const IPADDR = "127.0.0.1"
+
+var num_clients = 0
 
 func _ready() -> void:
 	var args = Array(OS.get_cmdline_args())
 	print(args)
-	var err = OK
 	if args.has("listen"):
-		print("switch over to server")
-		err = get_tree().change_scene_to_file(server_scene)
+		print("start a listen server")
+		multiplayer.peer_connected.connect(_peer_connected)
+		multiplayer.peer_disconnected.connect(_peer_disconnected)
+
+		var server = ENetMultiplayerPeer.new()
+		server.create_server(DEFAULT_PORT, MAX_CLIENTS)
+		multiplayer.set_multiplayer_peer(server)
+		$Label.text = "Server is listening"
+		$TextureRect.modulate = Color.BLUE
+		
 	elif args.has("join"):
-		print("switch over to client")
-		err = get_tree().change_scene_to_file(client_scene)
+		print("create a client")
+		multiplayer.connected_to_server.connect(self._connected_to_server)
+		multiplayer.server_disconnected.connect(self._server_disconnected)
+		multiplayer.connection_failed.connect(self._connection_failed)
+		var client = ENetMultiplayerPeer.new()
+		client.create_client(IPADDR, DEFAULT_PORT)
+		multiplayer.set_multiplayer_peer(client)
+		$Label.text = "created a client!"
+		$TextureRect.modulate = Color.ORANGE
 	else:
 		print("To run the client or server, pass \"listen\" or \"join\" as an argument.")
-	if err != OK:
-		push_error("mpstart -> scene load error: ", err)
+
+
 		
+
+func _connected_to_server():
+	print("Successfully connected to server")
+	$Label.text = "client connected!"
+
+
+func _server_disconnected():
+	print("Disconnected from server")
+	$Label.text = "client disconnected."
+
+
+func _connection_failed():
+	print("Connection to server failed!")
+	$Label.text = "client connection failed."
+
+func _peer_disconnected(id):
+	print("Peer " + str(id) + " has disconnected")
+	num_clients -= 1
+	$Label.text = "server: number of clients: %d" % num_clients
+
+func _peer_connected(id):
+	print("Peer " + str(id) + " has connected")
+	num_clients += 1
+	$Label.text = "server: number of clients: %d" % num_clients
