3e632e822070dc0770b344f1626ad0b88ca93f67
Zach Bernal <chrisknyfe@gmail.com>
2025-04-10 01:34:18 -0700

    Benchmark RefSerializer by KoBeWi

--- addons/picklegd/test/benchmark_test.gd
+++ addons/picklegd/test/benchmark_test.gd
@@ -9,13 +9,27 @@ const __source = 'res://addons/picklegd/pickler.gd'
 const ITERATIONS = 1000
 
 var pickler := Pickler.new()
-var bigdata := BigClassChrisknyfe.new()
+
+func before():
+	pickler.class_registry.clear()
+	RefSerializer._types.clear()
 
 func test_pickler_benchmark() -> void:
 	pickler.register_custom_class(CustomClassOne)
 	pickler.register_custom_class(CustomClassTwo)
 	pickler.register_custom_class(BigClassChrisknyfe)
 	
+	var bigdata := BigClassChrisknyfe.new()
 	for i in range(ITERATIONS):
 		var p = pickler.pickle(bigdata)
 		var u = pickler.unpickle(p)
+
+func test_refserializer_benchmark() -> void:
+	RefSerializer.register_type(&"CustomClassOne", CustomClassOne.new)
+	RefSerializer.register_type(&"CustomClassTwo", CustomClassTwo.new)
+	RefSerializer.register_type(&"BigClassChrisknyfe", BigClassChrisknyfe.new)
+	
+	var bigdata: BigClassChrisknyfe = RefSerializer.create_object(&"BigClassChrisknyfe")
+	for i in range(ITERATIONS):
+		var s = RefSerializer.serialize_object(bigdata)
+		var u = RefSerializer.deserialize_object(s)
